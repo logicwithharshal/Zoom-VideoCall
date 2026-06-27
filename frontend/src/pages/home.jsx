ad5e33bc5ca2ccd967a4d0a7f84c5185f64207e0
Harshal Raut <rharshal398@gmail.com>
2026-06-27 17:19:52 +0530

    Home Component incomplete

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import withAuth from '../utils/withAuth'
+import React, { useState } from "react";
+import withAuth from '../utils/withAuth';
+import { useNavigate } from 'react-router-dom';
 function HomeComponent(){
+
+    let navigate = useNavigate();
+    const [meetingCode, setMeetingCode] = useState("");
+    let handleJoinVideoCall = async () => {
+        navigate(`/${meetingCode}`)
+    }
+
     return(
-        <div>HomeComponent</div>
+        <div className="navBar">
+            <div style={{display:"flex", alignItems:"center"}}>
+                <h3></h3>
+            </div>
+        </div>
     )
 }
-export default withAuth(HomeComponent)4
+export default withAuth(HomeComponent)
